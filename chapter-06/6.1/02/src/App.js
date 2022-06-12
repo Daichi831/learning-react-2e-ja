ccaab788385dc1db3bfd91843cee5a6ba4960cc7
kanekodaichi <knkdic.bfc@gmail.com>
2022-06-12 20:43:16 +0900

    星がクリックされるたびにStarRatingコンポーネントが再描画されるようにした

--- chapter-06/6.1/02/src/App.js
+++ chapter-06/6.1/02/src/App.js
@@ -2,5 +2,5 @@ import React from "react";
 import StartRating from "./StarRating";
 
 export default function App() {
-  return <StartRating totalStars={10} />;
+  return <StartRating  />;
 }
--- chapter-06/6.1/02/src/StarRating.js
+++ chapter-06/6.1/02/src/StarRating.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
-const Star = ({ selected = false }) => (
-  <FaStar color={selected ? "red" : "grey"} />
+const Star = ({ selected = false, onSelect = (x) => x }) => (
+  <FaStar color={selected ? "red" : "grey"} onClick={onSelect}/>
 );
 
 export default function StarRating({ totalStars = 5 }) {
-  return [...Array(totalStars)].map((n, i) => <Star key={i} />);
+  const [selectedStars, setSelectedStars] = useState(0);
+  return (
+    <>
+      {[...Array(totalStars)].map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+          />
+      ))}
+      <p>
+        {selectedStars} of {totalStars} stars
+      </p>
+    </>
+  );
 }
